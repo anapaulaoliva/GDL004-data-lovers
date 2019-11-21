6351181fe8f8c7a535a6064496d1559778bcebfa
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2019-11-21 11:49:19 -0600

    Version 1 develop

--- src/main.js
+++ src/main.js
@@ -4,29 +4,35 @@ console.log(POKEMON);
 window.onload = () => {
 //funcion para que aparezca algo en el boton show
 const mostrarLista = () => {
-  const lista = document.createElement("DIV");
+
+  const lista = document.createElement("div");
   lista.classList.add("contenedor");
-  //lista.innerText = POKEMON[0].name + POKEMON[0].img;
-  //document.body.appendChild(lista);
+
   for (let i = 0; i < POKEMON.length; i++) {
-    let card = document.createElement("DIV");
+
+    let card = document.createElement("div");
     card.classList.add("card");
+    let margenCard = document.createElement("div");
+    margenCard.classList.add("margenCard");
+    card.appendChild(margenCard);
   //if(POKEMON[i].type === 'water'){
-    let descripcion = card.innerText = POKEMON[i].name ;
-    let img = document.createElement("IMG");
-    lista.appendChild(img);
+    let descripcion = margenCard.innerText = POKEMON[i].name + " " + POKEMON[i].type
+
+
+    let img = document.createElement("img");
     img.setAttribute('src', POKEMON[i].img);
-    let info = document.createElement("BUTTON");
+
+    let info = document.createElement("button");
     info.classList.add("infoBoton");
     info.innerText = "+ INFO ";
   //}
     document.body.appendChild(lista);
     lista.appendChild(card);
-    card.appendChild(img);
-    card.appendChild(info);
+    margenCard.appendChild(img);
+    margenCard.appendChild(info);
   }
 };
-document.querySelector(".tipos").addEventListener("click", mostrarLista);
+document.querySelector("#menu").addEventListener("click", mostrarLista);
 }
 
 //funcion para que se cree una lista que contiene los objetos
