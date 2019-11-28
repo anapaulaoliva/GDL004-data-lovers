400bd64fc2a1eb7596b25a5742e2e2491b6eae88
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2019-11-28 12:27:20 -0600

    Sort-scroll

--- src/data.js
+++ src/data.js
@@ -10,41 +10,52 @@ import POKEMON from './data/pokemon/pokemon.js';
 //parametro pokemones
 //callback retorna verdadero o falso
 
-
-/*
-export const filtradoPokemones = (pokemones) => {
- return POKEMON.filter((pokemones) => {
-  if (pokemones.type == "Grass");
-    console.log(filtradoPokemones);
-});
-}*/
-
-export const filtradoPokemones = (POKEMON, x) => {
+export const filtradoPokemones = (POKEMON, evento) => {
   return POKEMON.filter((pokemones) => {
-    if (pokemones.type.indexOf(x) !== -1){
+    if (pokemones.type.indexOf(evento) !== -1){
       return true;
     }
   });
 }
-
-
-
-
-/* Manejo de la e Data
- export const filterData = (data,condition) => { esta funcion filter o filtrar,
-recibiria la data y nos retornaria aquellos
-datos que si cumplan con la condicion.}
-
-export sortData(data, sortBy, sortOrder): esta funcion
- sort u ordenar recibe 3 parametros.
- #primer parametro Data nos entrega los datos
- #segundo parametro sortBy nos dice con respecto
- a cual de los campos de la data se quiere ordenar
- #sortOrder indica si se quiere ordenar
- de manera ascendente o descendente.
-
- deben ser puras e independientes del DOM
- deben ser exportadas en el boiler plate
- para ser usadas desde el archivo
- main.js, al cargar la pagina, (window.onload)
- u cada vez que el usuario interactue */
+export const pokemonesAZ = (POKEMON, name) => {
+  let resultadoAZ = [];
+  for (name in POKEMON) {
+    if (POKEMON.hasOwnProperty(name)) {
+      //console.log(POKEMON[name]);
+      resultadoAZ.push(POKEMON[name]);
+    }
+  }
+  resultadoAZ.sort(function(a,b) {
+    if (a.name < b.name ) {
+      return -1;
+    } else if (a.name > b.name){
+      return 1;
+    } 
+    })
+  .forEach(function(pokemonOrdenado){
+    return pokemonOrdenado;
+  });
+  //console.log(resultadoAZ);
+  return resultadoAZ;
+}
+export const pokemonesZA = (POKEMON, name) => {
+  let resultadoZA = [];
+  for (name in POKEMON) {
+    if (POKEMON.hasOwnProperty(name)){
+      console.log(POKEMON[name]);
+      resultadoZA.push(POKEMON[name]);
+    }
+  }
+  resultadoZA.sort(function(a,b) {
+    if (a.name > b.name ) {
+      return -1;
+    } else if (a.name < b.name){
+      return 1;
+    }
+  })
+  .forEach(function(pokemonOrdenado){
+    return pokemonOrdenado;
+  });
+  console.log(resultadoZA);
+ return resultadoZA;
+}
--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import POKEMON from'./data/pokemon/pokemon.js';
 console.log(POKEMON);
 import { filtradoPokemones } from './data.js';
+import { pokemonesAZ } from './data.js';
+import { pokemonesZA } from './data.js';
 
 window.onload = () => {
 const mostrarMenu = () => {
- let orderMenu = document.querySelector('.orderMenu').style.display="flex";
+ document.querySelector('.orderMenu').style.display="flex";
 
 }
 document.querySelector("#menu").addEventListener("mouseover", mostrarMenu);
-/*
-const ocultarMenu = () => {
+
+/*const ocultarMenu = () => {
  let orderMenu = document.querySelector('.orderMenu').style.display="none";
 
 }
 document.querySelector(".orderMenu").addEventListener("mouseout", ocultarMenu);
 */
 //funcion para que aparezca algo en el boton show
-const mostrarLista = () => {
+const mostrarLista = (POKEMON, evento, listaAZ, listaZA) => {
 document.querySelector("#contenedor").innerHTML = '';
 document.querySelector('.paginaInicio').style.display="none"
 document.querySelector('.topPokeContenedor').style.display="none";
@@ -36,7 +38,29 @@ document.querySelector('.topPokeContenedor').style.display="none";
    document.querySelector("#contenedor").appendChild(card);
   })
 };
-document.querySelector(".todos").addEventListener("click", mostrarLista);
+document.querySelector(".todos").addEventListener("click", () => {
+  mostrarLista(POKEMON);
+});
+//funcion para ordenar A-Z y Z-A
+
+document.querySelector(".A-Z").addEventListener("click", () => {
+  ordenarListaAZ();
+});
+const ordenarListaAZ = () => {
+  let listaAZ;
+  listaAZ = pokemonesAZ(POKEMON, name);
+  console.log(listaAZ);
+  mostrarLista(listaAZ);
+};
+document.querySelector(".Z-A").addEventListener("click", () => {
+  ordenarListaZA();
+});
+const ordenarListaZA = () => {
+  let listaZA;
+  listaZA = pokemonesZA(POKEMON, name);
+  console.log(listaZA);
+  mostrarLista(listaZA);
+};
 //funcion para que se cree una lista que contiene los objetos
 
 //parametro e que es el evento
@@ -61,19 +85,18 @@ function mostrarFiltrado(e) {
   document.querySelector('#contenedor').style.display="flex";
   document.querySelector('.topPokeContenedor').style.display="none";
 //cuando se de click al event listener, se ira al elemento y obtendra el id de ese elemento.
-let paginaInicio = document.querySelector('.paginaInicio');
-paginaInicio.style.display="none";
-const x = e.target.id;
-console.log(x);
+document.querySelector('.paginaInicio').style.display="none";
+const evento = e.target.id;
+console.log(evento);
 //ocultar la pagina de inicio al dar click a los pokemones
 //guardados todos los datos filtrados
-const pokemonesFiltrados = filtradoPokemones(POKEMON, x);
+const pokemonesFiltrados = filtradoPokemones(POKEMON, evento);
 console.log(pokemonesFiltrados);
 //document.querySelector("#filtrado").innerHTML = ' ';
 pokemonesFiltrados.map((pokemonFiltrado) => {
   let card = document.createElement("div");
   const template = `
-  <div class="filtrado">
+  <div class="contenedor">
   <div class="card">
   <span style="text-transform: uppercase;">${pokemonFiltrado.name}</span>
   <span style="text-transform: uppercase;">${pokemonFiltrado.type}</span>
@@ -89,27 +112,42 @@ pokemonesFiltrados.map((pokemonFiltrado) => {
 
 //funcion para boton comenzar de la pagina de Inicio.
 const mostrarTopPoke = () => {
- let paginaInicio = document.querySelector('.paginaInicio');
- let paginaTopPokemos = document.querySelector('.topPokeContenedor');
-
- paginaInicio.style.display="none";
- paginaTopPokemos.style.display="flex";
+ document.querySelector('.paginaInicio').style.display="none";
+ document.querySelector('.topPokeContenedor').style.display="flex";
 
 }
 document.querySelector('#botonComenzar').addEventListener('click', mostrarTopPoke);
 
 const paginaInicio = () => {
-  let paginaInicio = document.querySelector('.paginaInicio');
-  let paginaTopPokemos = document.querySelector('.topPokeContenedor');
-  let contenedor =  document.querySelector('#contenedor');
-
-  paginaInicio.style.display="flex";
-  paginaTopPokemos.style.display="none";
-  contenedor.style.display="none";
+  document.querySelector('.paginaInicio').style.display="flex";
+  document.querySelector('.topPokeContenedor').style.display="none";
+  document.querySelector('#contenedor').style.display="none";
 
 }
 document.querySelector('#pokebola').addEventListener('click', paginaInicio);
+//slideshow
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every "" seconds
+}
 
-
+document.getElementById("pokeUno").addEventListener("click", mostrarLista);
+document.getElementById("pokeDos").addEventListener("click", mostrarLista);
+document.getElementById("pokeTres").addEventListener("click", mostrarLista);
+document.getElementById("pokeCuatro").addEventListener("click", mostrarLista);
 }
